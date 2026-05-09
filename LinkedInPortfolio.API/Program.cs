9e21511b7f36429f4abbccc8601adfba8711d28d
Ahmed Omran <eng.ahmed.3mranr@email.com>
2026-05-10 01:43:12 +0300

    feat: wire DI, CORS, and auto-migration in Program.cs

--- LinkedInPortfolio.API/Program.cs
+++ LinkedInPortfolio.API/Program.cs
@@ -1,3 +1,42 @@
+using LinkedInPortfolio.API.Data;
+using LinkedInPortfolio.API.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IProfileService, ProfileService>();
+builder.Services.AddScoped<ILinkedInScraperService, LinkedInScraperService>();
+builder.Services.AddHttpClient();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReact", policy =>
+        policy.WithOrigins("http://localhost:5173", "https://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowReact");
+app.MapControllers();
 app.Run();
---
+++ LinkedInPortfolio.API/Services/LinkedInScraperService.cs
@@ -0,0 +1,7 @@
+namespace LinkedInPortfolio.API.Services;
+
+public class LinkedInScraperService : ILinkedInScraperService
+{
+    public Task<ProfileData> ScrapeProfileAsync()
+        => throw new NotImplementedException("Scraper not yet implemented");
+}
